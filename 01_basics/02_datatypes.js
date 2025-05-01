52ae0a439981491e6912f49c89fd4cce8b883786
Aman77d <amanmishra13062003@gmail.com>
2025-05-01 15:22:21 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,30 @@
+"use strict"// it provide the newer version of nodejs.
+
+//alert("hello") // we are using nodejs, not browser.
+
+console.log(3 
+    + 
+    3) // code readability should be high
+
+console.log("aman")
+
+/* tc39 and mdn these two site for documentation of java script*/
+
+let name="aman"
+let age=18
+let userLogin=true
+
+// string
+// number 2 to power of 53 its is a range of number(interger).
+// bigint => it use for large number
+// boolean => true / false
+// null => it is standalone value
+// undefine => not assign the value
+// symbol => it use in react and use for unique.
+
+
+// object =>
+
+console.log(typeof undefined);  // it give typeof is undefined
+
+console.log(typeof null) // it give typeof is Object
