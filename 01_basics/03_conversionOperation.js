2d7a99a821934eeb7352ea5edd07654e8c004a69
Aman77d <amanmishra13062003@gmail.com>
2025-09-17 08:13:15 +0000

    modified

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,7 +1,7 @@
 let score = "33abx"
 
-console.log(typeof score);
-console.log(typeof score);
+//console.log(typeof score);
+//console.log(typeof score);
 
 // let score = "33" // it gives number
 // let score = "33abc" // it gives nan(not a number)
@@ -12,13 +12,13 @@ console.log(typeof score);
 
 
 let valueInNumber = Number(score)
-console.log(valueInNumber);
-console.log(typeof valueInNumber)
+//console.log(valueInNumber);
+//console.log(typeof valueInNumber)
 
 let userLoggendIn = 1
 
 let userLoggedOut = Boolean(userLoggendIn)
-console.log(userLoggedOut)
+//console.log(userLoggedOut)
 
 // 1 => true ; // 0 => false 
 // "" => false 
@@ -29,7 +29,65 @@ let someNumber = 33
 
 let stringNumber = String(someNumber)
 
-console.log(stringNumber)
-console.log(typeof stringNumber)
+//console.log(stringNumber)
+//console.log(typeof stringNumber)
 
 // 33 => string 
+
+//************ Operations ******************
+
+let value = 3
+
+let negValue = -value
+//console.log(negValue);  // it give negative number value
+
+/*
+console.log(2+2); // addition 
+console.log(2-2); // subtract 
+console.log(2*2); // multiply
+console.log(2**3); // power 
+console.log(2/2); // division 
+console.log(2%3); // reminder or modulus 
+*/
+
+
+let str1 = "hello"
+let str2 = " Aman"
+
+let str3 = str1 + str2
+//console.log(str3);
+
+/*
+console.log("1" + 2);   // it is behave like a string 
+console.log(1 + "2");   // it is also behave like a string 
+console.log("1" + 2 + 2);  // if the first number in string it is behave all operation in string
+console.log(1 + 2 +"2");  // but if first number is number than add all the number and add the last string number
+console.log(true);
+console.log(+true);
+*/
+
+
+let num1 , num2 ,num3 ;
+num1 = num2 = num3 = 2 + 2;
+
+
+let gameConter = 100;
+console.log(++gameConter);
+console.log(gameConter++);
+
+// both increment and decrement value output is 101 but its different 
+// assignment : study about prefix and postfix increment 
+
+
+
+ 
+
+
+
+ 
+
+
+
+
+
+
