bcb37dfc4e996930e7b86f1bd1631ef935f43348
Aman77d <amanmishra13062003@gmail.com>
2025-09-18 11:02:16 +0000

    comparision

---
+++ 01_basics/04_conversion.js
@@ -0,0 +1,35 @@
+//console.log(2 > 1);
+//console.log(2 >= 1);
+//console.log(2< 1);
+//console.log(2 == 2);
+//console.log( 2 != 3);
+
+/*
+console.log("2" > 1);  // java script automaticaly change string to number and compare as a number 
+console.log("02" > 1);
+*/
+
+
+console.log(null > 0);  // at the time of comparision java script 
+console.log(null == 0);
+console.log(null >= 0);
+
+
+console.log(undefined > 0);
+console.log(undefined == 0);
+console.log(undefined >= 0);
+
+// null and undefined make some confusion this is language problem it create unconsistency
+
+// (===) it is stricty check operation known as triple equalto 
+//it compare the number but also check the data type of current number for example.....
+
+console.log("2"  === 1);  // false 
+console.log(2 === 2); // true 
+
+
+
+
+
+
+
