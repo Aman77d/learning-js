2db80b5c2ac395b2bdebdf894ba0706e01510f75
Aman77d <amanmishra13062003@gmail.com>
2025-09-19 06:24:30 +0000

    datatype summary and memory area

--- 01_basics/datatype_summary.js
+++ 01_basics/datatype_summary.js
@@ -19,12 +19,12 @@ let userEmail ;   // undefined
 
 const id = Symbol('123')
 const anotherId = Symbol('123')
-console.log(typeof id);    // symbol
+//console.log(typeof id);    // symbol
 
 console.log(id === anotherId);
 
 const bigNumber = 1212154145454521n  
-console.log(typeof bigNumber);   // BigInt 
+//console.log(typeof bigNumber);   // BigInt 
 
 
 
@@ -35,14 +35,14 @@ console.log(typeof bigNumber);   // BigInt
 // 3 types : array , function , objects 
 
 const heroes = ["Iron man" , " Thor " , "Loki"];
-console.log(typeof heroes);     // object 
+//console.log(typeof heroes);     // object 
 
 
 let myObj =  {
     name: "aman" ,
     age: 22
 }
-console.log(typeof myObj) // object 
+//console.log(typeof myObj) // object 
 
 const myFunction = function(){
 
@@ -50,4 +50,41 @@ const myFunction = function(){
 
 }
 
-console.log(typeof myFunction);  // function 
+//console.log(typeof myFunction);  // function 
+
+// ****************** memory in javascript *****************
+
+// primitive( stack )    ,    non-primitive ( Heap)
+/*
+ stack create the copy of the refernce variable
+ but if you chnage second variable value .....original is same ...
+*/
+
+
+/*
+// heap not create the copy it give direct acess to the other varibale 
+but in heap it will change the original value if you change the second variable value
+*/
+
+
+
+let myName = "Aman mishra"  // original name is not changed
+let myotherName = myName
+
+myotherName = "Tushar mishra"
+
+console.log(myName);
+console.log(myotherName);
+
+let userOne = {
+    email : "aman123@.com" ,
+    upi : "aman784@lo"
+}
+
+let userTwo = userOne;
+userTwo.email= "tushar125@.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
+
+// it change the original value because non-primitive datatype store inside the Heap 
