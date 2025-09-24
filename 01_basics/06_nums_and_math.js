130421ec25c26ac796f591012ecca7a77b003e67
Aman77d <amanmishra13062003@gmail.com>
2025-09-24 06:43:41 +0000

    number and math

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,41 @@
+const score = 400 ;
+//console.log(score);
+
+const balance = new Number(100);
+//console.log(balance);
+
+//console.log(balance.toString());
+//console.log(balance.toFixed(2));
+
+const otherNumber = 13.3658
+//console.log(otherNumber.toPrecision(3));
+
+const hundred = 1000000;
+//console.log(hundred.toLocaleString('en-In'));
+
+
+//---------------------Maths -------------------------------------------
+
+//console.log(Math);
+/*
+console.log(Math.abs(-6));
+console.log(Math.round(12.2));
+console.log(Math.ceil(10.3));
+console.log(Math.floor(8.9));
+console.log(Math.min(5,3,9,7));
+console.log(Math.max(10,3,78,9));
+*/
+
+console.log(Math.random());
+console.log(Math.random() * 10  + 1 );
+console.log(Math.floor(Math.random()*10)+1);
+
+
+const min = 10
+const max = 20 
+
+console.log(Math.floor(Math.random() * (max - min +1)) + min);
+
+
+
+
