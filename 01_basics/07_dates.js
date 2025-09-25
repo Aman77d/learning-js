82e4df527dcb2db8a6ef812ac819e1212af76c8d
Aman77d <amanmishra13062003@gmail.com>
2025-09-25 08:28:37 +0000

    dates

---
+++ 01_basics/07_dates.js
@@ -0,0 +1,26 @@
+// Dates
+
+const myDate = new Date()
+//console.log(myDate.toString());
+
+console.log(myDate.toDateString());
+// day month present date and current year
+
+//console.log(myDate.toISOString());
+
+//console.log(myDate.toJSON());
+
+console.log(myDate.toLocaleDateString());
+// 9/25/2025 (month / date / year)
+
+//console.log(myDate.toLocaleTimeString());
+
+//console.log(typeof myDate);
+
+
+//let myCreatedDate = new Date(2024 , 0  , 26)
+//let myCreatedDate = new Date(2024 , 0  , 26 , 5 , 30)
+let myCreatedDate = new Date("2025-06-13")
+console.log(myCreatedDate.toLocaleString());
+
+
