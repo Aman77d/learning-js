26bc14e5c3471ea5f1ea9fb34d25c359e8c20ae2
Aman77d <amanmishra13062003@gmail.com>
2026-07-22 06:19:02 +0000

    JS file

---
+++ 07_projects/06_unlimited color/script.js
@@ -0,0 +1,34 @@
+// genrates random background color
+const randomColor = function(){
+    const hex = "0123456789ABCDEF"
+    let color = "#"
+    for(let i = 0 ; i < 6 ; i++ ){
+        color += hex[Math.floor(Math.random() * 16)]
+    }
+    return color
+    
+}
+
+// console.log(randomColor());
+
+let intervalId;
+const startChanginColor = function(){
+
+    if(!intervalId){
+        intervalId = setInterval(changebgColor , 1000)
+    }
+    
+     function changebgColor(){
+        document.body.style.backgroundColor = randomColor();
+    }
+}
+
+
+const stopChanginColor = function(){
+    clearInterval(intervalId)
+    intervalId = null
+}
+
+document.querySelector('#start').addEventListener('click' , startChanginColor)
+
+document.querySelector('#stop').addEventListener('click' ,stopChanginColor )
